22184209ae4dc4599fb34d34c3fa7697f9707b5a
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-01-18 11:18:02 +0100

    Update sticky.js

--- js/sticky.js
+++ js/sticky.js
@@ -1,1 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var header = document.querySelector('.l-header');
+    var scrollPosition = window.scrollY;
 
+    function addRemoveScrolledClass() {
+        scrollPosition = window.scrollY;
+
+        if (scrollPosition > 0) {
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    }
+
+    // Add the scroll event-listener
+    window.addEventListener('scroll', addRemoveScrolledClass);
+
+    // Call the function once to set initial state
+    addRemoveScrolledClass();
+});
